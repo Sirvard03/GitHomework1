679d53e026ac7085f5d9de751b0f1ebf3c51ef09
Unnatipate <92794626+Unnatipate@users.noreply.github.com>
2021-10-20 13:17:59 +0100

    Add files via upload

---
+++ GitHomework1.java
@@ -0,0 +1,8 @@
+public class GitHomework1 {
+    public static void main(String[] args){
+        System.out.println("I Like Git");
+        System.out.println("I Like Git");
+        System.out.println("I Like Git");
+    }
+
+}
---
+++ GitHomework3.java
@@ -0,0 +1,10 @@
+public class GitHomework3 {
+    public static void main(String[] args) {
+        System.out.println("I Like Git");
+        System.out.println("I Like Git");
+        System.out.println("I Like Git");
+        System.out.println("I Like Git");
+        System.out.println("I Like Git");
+    }
+
+    }
